7fd3d9161ecf0c6c225ae42820f05fc8397bd8ab
Ive Romão <ive_net@hotmail.com>
2022-12-22 23:09:12 -0300

    aula json

--- js/script.js
+++ js/script.js
@@ -20,41 +20,36 @@ formulario.addEventListener('submit', (e) => {
         formulario.reset();
 
     } else {
-        if (hospedes.value == "") {
-            alert('Quantidade de hóspedes inválida');
-
-        } else {
-            class Hospedagem {
-                constructor(quantidadeDiarias, quantidadeHospedes, precoDiaria) {
-                    this.quantidadeDiarias = quantidadeDiarias;
-                    this.quantidadeHospedes = quantidadeHospedes;
-                    this.precoDiaria = precoDiaria;
-                }
-            
-                calcularCusto() {
-                    let custoDiaria = this.quantidadeDiarias * this.quantidadeHospedes * this.precoDiaria;
-            
-                    let dadosHospedagem = document.createElement("div");
-            
-                    dadosHospedagem.innerHTML = `<h3>Dados da Hospedagem:</h3>
-                                            <p><br>Data de entrada: ${dataEntradaBrasileira}</p> 
-                                            <p>Data de saida: ${dataSaidaBrasileira}</p> 
-                                            <p>Quantidade de diárias: ${this.quantidadeDiarias}</p> 
-                                            <p>Quantidade de hóspedes: ${this.quantidadeHospedes}</p>
-                                            <p><b>Custo total das diárias: ${(new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(custoDiaria))}</b></p>`;
-                                            
-                    document.body.appendChild(dadosHospedagem);
-                }
+        class Hospedagem {
+            constructor(quantidadeDiarias, quantidadeHospedes, precoDiaria) {
+                this.quantidadeDiarias = quantidadeDiarias;
+                this.quantidadeHospedes = quantidadeHospedes;
+                this.precoDiaria = precoDiaria;
             }
-            
-            const hospedagens = [];
-            
-            hospedagens.push(new Hospedagem (calculaDiarias(dataEntrada.value, dataSaida.value), hospedes.value, 300));
         
-            hospedagens.forEach(hospedagem => hospedagem.calcularCusto());
+            calcularCusto() {
+                let custoDiaria = this.quantidadeDiarias * this.quantidadeHospedes * this.precoDiaria;
+        
+                let dadosHospedagem = document.createElement("div");
+        
+                dadosHospedagem.innerHTML = `<h3>Dados da Hospedagem:</h3>
+                                        <p><br>Data de entrada: ${dataEntradaBrasileira}</p> 
+                                        <p>Data de saida: ${dataSaidaBrasileira}</p> 
+                                        <p>Quantidade de diárias: ${this.quantidadeDiarias}</p> 
+                                        <p>Quantidade de hóspedes: ${this.quantidadeHospedes}</p>
+                                        <p><b>Custo total das diárias: ${(new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(custoDiaria))}</b></p>`;
+                                        
+                document.body.appendChild(dadosHospedagem);
+            }
         }
-    }
+        
+        const hospedagens = [];
+        hospedagens.push(new Hospedagem (calculaDiarias(dataEntrada.value, dataSaida.value), hospedes.value, 300));
+        hospedagens.forEach(hospedagem => hospedagem.calcularCusto());
 
+        const hospedagensJson = JSON.stringify({checkin: dataEntrada.value, checkout: dataSaida.value, hospedes: hospedes.value});
+        localStorage.setItem('hospedagem', hospedagensJson);
+    }
 });
 
 function calculaDiarias(a, b) {
